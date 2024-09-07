8d25eb50e910931f0e625258e735ec7b989ec0e9
lantiantx <125938094+lantiantx@users.noreply.github.com>
2024-09-07 14:19:55 +0800

    common.rs

--- src/common.rs
+++ src/common.rs
@@ -894,7 +894,7 @@ pub fn get_api_server(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://op.txnh9919.cn:7000".to_owned()
+    "http://op.txnh9919.cn:7000".to_owned()
 }
 
 pub fn get_audit_server(api: String, custom: String, typ: String) -> String {
